d8e88f37d5ab81bc74d1aeb6eb0bdc8f9f375ef5
Harshit Mehta <harshit.mehta@live.com>
2021-02-25 20:07:29 +0800

    initial

---
+++ src/js/const.js
@@ -0,0 +1,14 @@
+export const APP_ID = '856DE0C3-53F5-471A-A18A-8D6F9A24E77C';
+export const USER_ID = 'user_id';
+export const DISPLAY_NONE = 'none';
+export const DISPLAY_BLOCK = 'block';
+export const DISPLAY_FLEX = 'flex';
+export const ACTIVE_CLASSNAME = 'active';
+export const KEY_ENTER = 13;
+export const FILE_ID = 'attach_file_id';
+export const UPDATE_INTERVAL_TIME = 5 * 1000;
+export const COLOR_RED = '#DC5960';
+export const MESSAGE_REQ_ID = 'reqId';
+export const OPEN_CHANNEL_SEARCH_URL = 'search_open_channel';
+
+export const body = document.querySelector('body');
