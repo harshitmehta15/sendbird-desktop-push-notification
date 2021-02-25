d0507da7b5a40a9642b4da52c4c63ef13ecf0ec9
Harshit Mehta <harshit.mehta@live.com>
2021-02-26 04:11:19 +0800

    syntax fixes

--- server.js
+++ server.js
@@ -10,10 +10,39 @@ app.get('/', function(req, res) {
   res.sendfile('index.html');
 });
 
-app.post("/profanity", (req, res) => {
-  console.log(req.body) 
-  res.status(200).end() // Responding is important
-})
-
 app.listen(process.env.PORT || PORT);
 console.log(`[SERVER RUNNING] 127.0.0.1:${PORT}`);
+
+
+app.post("/profanity", (req, res) => {
+  console.log("pakad liya"); 
+  console.log(req.body); 
+  pushNotification();
+  res.status(200).end(); // Responding is important
+});
+
+function pushNotification(){
+  // Let's check if the browser supports notifications
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  }
+
+  // Let's check whether notification permissions have already been granted
+  else if (Notification.permission === "granted") {
+    // If it's okay let's create a notification
+    var notification = new Notification("Hi there!");
+  }
+
+  // Otherwise, we need to ask the user for permission
+  else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      // If the user accepts, let's create a notification
+      if (permission === "granted") {
+        var notification = new Notification("Hi there!");
+      }
+    });
+  }
+
+  // At last, if the user has denied notifications, and you
+  // want to be respectful there is no need to bother them any more.
+}
